0318ea5c5e00dcd36c0731c5c85e0d7f05e56e5a
Zowarke <jonathan.cortinovis@hotmail.fr>
2015-03-13 14:31:50 +0100

    reset -> fichier prêts à l'emploi
    remplir le index.

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php 
  include("Header.php");
 ?>
-	<body id="index">
-		<header>
-			<h1>Gridly <img src="./Ressources/img/rond.png"></h1>
-			<ul>
-				<li><a href="">home</a></li>
-				<li><a href="">digital designs</a></li>
-				<li><a href="">web designs</a></li>
-				<li><a href="">branding/Identity</a></li>
-				<li><a href="">about me</a></li>
-				<li><a href="">contact</a></li>
-			</ul>
-		</header>
-		
+	<body>
 	</body>
 <?php
 include("Footer.php");
