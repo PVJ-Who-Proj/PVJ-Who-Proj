236a756a6eb1c32f6c04d886ab5c69b657dae539
Zowarke <jonathan.cortinovis@hotmail.fr>
2015-03-13 14:13:33 +0100

    Implémentation de "index"
    Voici l'implémentation de la page index, avec include pour header et
    footer.

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-gzrgerh
+<?php 
+ include("Header.php");
+?>
+	<body id="index">
+		<header>
+			<h1>Gridly <img src="./Ressources/img/rond.png"></h1>
+			<ul>
+				<li><a href="">home</a></li>
+				<li><a href="">digital designs</a></li>
+				<li><a href="">web designs</a></li>
+				<li><a href="">branding/Identity</a></li>
+				<li><a href="">about me</a></li>
+				<li><a href="">contact</a></li>
+			</ul>
+		</header>
+		
+	</body>
+<?php
+include("Footer.php");
+?>
