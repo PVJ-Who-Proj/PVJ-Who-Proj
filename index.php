6827a5f1f4a7314abde8f27c9e1290b87ff772ca
Reflobapi <clayette.pierre@gmail.com>
2015-03-17 11:13:31 +0100

    QQQ

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 	<div class="welcome" >
 		<div>
 			<p class="welcome_para"> Welcome in Guess Who! Through this game, you are going to play with your friends or against the computer. It will be a very funny experience for you and your family!! </p>
-			<p class="welcome_para"> Just click the Start button to play! </p>
+			<p class="welcome_para"> Just click the following button to continue! </p>
+			<a id="welcome_next_butt" href=""><img  src="Ressources/img/next.png"></a>
 		</div>
 		<img src="Ressources/img/welcome.png">
 
