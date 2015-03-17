d69ead65e1afe5b9f55ebe74efba09161ac3f54f
Reflobapi <clayette.pierre@gmail.com>
2015-03-17 10:38:45 +0100

    zizi

--- Header.php
+++ Header.php
@@ -5,5 +5,14 @@
 		<meta charset="utf-8"/>
 		<meta name="description" content="Article and picture blog"/>
 		<link rel="stylesheet" type="text/css" href="GuessWho.css "/>
+		<link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css'>
 		<title>PVJGuessWho.com</title>
 	</head>
+	<body>
+		<section class="integral">
+			<div class="logo">
+				<img src="Ressources/img/Logo_Guess_Who.png">
+			</div>
+
+			
+
--- index.php
+++ index.php
@@ -1,14 +1,19 @@
+
 <?php 
+
  include("Header.php");
 ?>
-	<body>
-		<section>
-			<img id="logo" src="./Ressources/img/logo_guess_who.png">
+	<div class="welcome" >
+		<div>
+			<p class="welcome_para"> Welcome in Guess Who! Through this game, you are going to play with your friends or against the computer. It will be a very funny experience for you and your family!! </p>
+			<p class="welcome_para"> Just click the Start button to play! </p>
+		</div>
+		<img src="Ressources/img/welcome.png">
 
-			<img id="paneau" src="./Ressources/img/paneau.png">
+	</div>
 
-		</section>
-	</body>
+
+	
 <?php
 include("Footer.php");
 ?>
