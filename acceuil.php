276197c5608bf56f3f8cd73f3c91885ccb6ab116
Reflobapi <clayette.pierre@gmail.com>
2015-03-17 21:05:56 +0100

    poil de gitub

---
+++ acceuil.php
@@ -0,0 +1,37 @@
+<?php
+include("Header.php")
+?>
+<div class="welcome">
+	<div>
+		<ol class="liste_acceuil">
+			<a href="">
+				<li id="login_liste_acceuil">
+					Login!
+				</li>
+			</a>
+			<a href="">
+				<li>
+					Create your own account!
+				</li>
+			</a>
+			<a href="">
+				<li>
+					Check ranking
+				</li>
+			</a>
+			<a href="">
+				<li>
+					Read the rules here!
+				</li>
+			</a>
+		</ol>
+		<a class="small_back_butt" href="index.php"><img  src="Ressources/img/small_back.png"></a>
+	</div>
+
+	<img src="Ressources/img/before.png">
+</div>
+
+
+<?php
+include("Footer.php");
+?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<div>
 			<p class="welcome_para"> Welcome in Guess Who! Through this game, you are going to play with your friends or against the computer. It will be a very funny experience for you and your family!! </p>
 			<p class="welcome_para"> Just click the following button to continue! </p>
-			<a id="welcome_next_butt" href=""><img  src="Ressources/img/next.png"></a>
+			<a class="next_butt" href="acceuil.php"><img  src="Ressources/img/next.png"></a>
 		</div>
 		<img src="Ressources/img/welcome.png">
 
