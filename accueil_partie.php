8e91e32234f05aeba2d3c8507735b7b0db3205fa
vincep69 <vince.pla@laposte.net>
2015-03-26 12:54:08 +0100

    voila le bon truc

--- accueil_partie.php
+++ accueil_partie.php
@@ -8,11 +8,13 @@
 
 	<div class="entour_menu">
 		<a id="create_game" href="">Create a game</a>
-		<a id="previous_game" href="">My previous games</a>
-		<a id="running_games" href="">My running games</a>
+		<a class="taillecube" href="">
+			<p id="previous_game" >My<br>previous<br>games</p>
+		</a>
+
+		<a class="taillecube2" href="">
+			<p id="running_games" href="">My<br>running<br>games</p>
+		</a>
+		<a id="create_game" href="">Join game</a>
 	</div>
-
-
-
-
  </div>
