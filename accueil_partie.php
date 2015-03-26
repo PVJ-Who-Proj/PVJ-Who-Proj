dd48e2a540e460079557e59679ef7ea6a0111bee
Reflobapi <clayette.pierre@gmail.com>
2015-03-26 14:05:53 +0100

    csq

--- accueil_partie.php
+++ accueil_partie.php
@@ -8,7 +8,7 @@ include("Header.php");
  <div class="welcome" id="background_menu">
 
 	<div class="entour_menu">
-		<a id="create_game" href="">Create a game</a>
+		<a id="create_game" href="create_game.php">Create a game</a>
 		<a class="taillecube" href="">
 			<p id="previous_game" >My<br>previous<br>games</p>
 		</a>
