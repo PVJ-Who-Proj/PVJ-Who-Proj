d008242f433d8738c49064fee66b60da495f2fb0
vincep69 <vince.pla@laposte.net>
2015-03-30 09:37:08 +0200

    login secure

--- creation_compte_inter.php
+++ creation_compte_inter.php
@@ -6,24 +6,18 @@ $login = mysql_real_escape_string($_POST['creer_login']);
 $password = mysql_real_escape_string($_POST['creer_mdp']);
 $confirm = mysql_real_escape_string($_POST['confirm_mdp']);
 $adr_mail = mysql_real_escape_string($_POST['adr_mail']);
-$login = addslashes($login);
-$password = addslashes($password);
-$confirm = addslashes($confirm);
-$adr_mail = addslashes($adr_mail);
-$login = htmlspecialchars($login);
-$password = htmlspecialchars($password);
-$confirm = htmlspecialchars($confirm);
-$adr_mail = htmlspecialchars($adr_mail);
 
-if(empty($password) || empty($login) || empty($adr_mail)){
+if(empty($password) || empty($login) || empty($confirm) || empty($adr_mail))
+{
 	$_SESSION['mdp_vide'] = false;
 	header('location: creation_compte.php');
+	exit;
 }
 
 if($password <> $confirm)
 {
 	$_SESSION['mdp_correct'] = false;
-	header('Location: creation_compte.php');	
+	header('Location: creation_compte.php');
 }
 else
 {
