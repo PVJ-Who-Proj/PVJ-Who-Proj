23b9d15891fa05806c043f71ccec2e58cbbf104b
vincep69 <vince.pla@laposte.net>
2015-04-03 15:47:47 +0200

    boutons création game

--- create_game.php
+++ create_game.php
@@ -7,14 +7,12 @@ include("Header.php");
 	<form class="create_game" action="create_game_inter.php" method="POST">
 		<input type="text" name="game_title" placeholder="Name of the game">
 
-
 		<div class="create_game_checkbox">
 			<input id="carre" type="checkbox" name="type_game" value="Two players"/><label for="carre"> Real player</label><br>
 			<input id="carrebis" type="checkbox" name="type_game" value="Computer"/><label for="carrebis"> Computer</label> 
 		</div>
 
-
-		<div class="zonebas_creategame">
+		<div class="back_butt_block zone_bas_create_game">
 			<a class="back_butt_login" id="back_rank" href="accueil_partie.php"><img  src="Ressources/img/back.png"></a>
 			<input class="butt_create_game" type="submit" value="">
 		</div>
