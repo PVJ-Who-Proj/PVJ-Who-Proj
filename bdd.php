6a41f41f1de92609c2ede425907ebe889e23dae8
Zowarke <jonathan.cortinovis@hotmail.fr>
2015-04-24 22:48:53 +0200

    PAtch

--- bdd.php
+++ bdd.php
@@ -1,26 +1,13 @@
 <?php
-<<<<<<< HEAD
-try
-=======
 
 try //appelle de BDD
->>>>>>> origin/master
 {
 	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 	$bdd = new PDO('mysql:host=localhost;dbname=guesswho','root', '', $pdo_options);
-
 }
-<<<<<<< HEAD
 
 catch(Exception $e)
 {
 	die('Error: '.$e->getMessage());
 }
-=======
-catch(Exception $e) //capture du message d'erreur en cas d'erreur
-{
-	die('Error: '.$e->getMessage());
-}
-
->>>>>>> origin/master
 ?>
